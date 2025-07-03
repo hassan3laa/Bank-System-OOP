d57f1fefccde9672d0c393042cd8800210177f5a
Hassan alaa <hassanalaa1400@gmail.com>
2025-07-04 01:12:34 +0300

    added anthor two classes

--- Bank System/Bank System/Source.cpp
+++ Bank System/Bank System/Source.cpp
@@ -10,30 +10,55 @@ using ll = long long;
 
 
 class Bank {
-public :
-	static int totalAcc;
+public:
+    static int totalAcc;
 
 };
 int Bank::totalAcc = 0;
 
 class Account {
-private:
-	int accnum;
-	string ownerName;
-	int balance;
+protected:
+    int accnum;
+    string ownerName;
+    double balance;
 
 public:
-	Account(int an, string ow, int b) : accnum(an), ownerName(ow), balance(b) {
-		Bank::totalAcc++;
-	}
+    Account(int an, string ow, double b) : accnum(an), ownerName(ow), balance(b) {
+        Bank::totalAcc++;
+    }
+    virtual void deposit(double am) {
+        if (am > 0) {
+            balance += am;
+        }
+        else {
+            cout << "Invalid Amount";
+        }
+    }
+    virtual void withdraw(double am) = 0;
+    virtual void display_info() const = 0;
+
+    int get_accnum() {
+        return accnum;
+    }
+    double get_balance() {
+        return balance;
+    }
+    friend void monthly_fee(Account& test);
 
 
 };
+class current_acc : public Account {
 
+};
+class saving_acc : public Account {
 
-
+};
+void monthly_fee(Account& test) {
+    test.balance -= 20;
+}
 
 
 int main() {
+
 
 }
